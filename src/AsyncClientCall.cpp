6af597807ea4b0dbdaf09542b07ea9434d021cea
Mrucznik <mrucznix@gmail.com>
2020-05-21 20:06:44 +0200

    Merge impl and declaration (It is not possible to write the implementation of a template class in a separate cpp file and compile.)

--- src/AsyncClientCall.cpp
+++ src/AsyncClientCall.cpp
@@ -3,22 +3,3 @@
 //
 
 #include "AsyncClientCall.h"
-
-template<class T>
-void AsyncClientCall<T>::Execute() {
-    if (this->status.ok())
-    {
-        CError<CCallback> callback_error;
-        std::shared_ptr<CCallback> callback = GetCallback(*this, callback_error);
-        if (callback_error && callback_error.type() != CCallback::Error::EMPTY_NAME) {
-            logprintf("create err %s", callback_error.msg().c_str());
-            return;
-        }
-
-        callback->Execute();
-    }
-    else
-    {
-        logprintf("rpc failed");
-    }
-}
