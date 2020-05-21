8e0b9ca99904c68f8ce88e61759b6ed0b227148d
Mrucznik <mrucznix@gmail.com>
2020-05-21 19:38:34 +0200

    Split declaration and implementation

--- src/AsyncClientCall.h
+++ src/AsyncClientCall.h
@@ -30,23 +30,7 @@ public:
                     const char *callbackName, const char *callbackFormat)
             : callbackName(callbackName), callbackFormat(callbackFormat), GetCallback(getCallback) {}
 
-    void Execute() override {
-        if (this->status.ok())
-        {
-            CError<CCallback> callback_error;
-            std::shared_ptr<CCallback> callback = GetCallback(*this, callback_error);
-            if (callback_error && callback_error.type() != CCallback::Error::EMPTY_NAME) {
-                logprintf("create err %s", callback_error.msg().c_str());
-                return;
-            }
-
-            callback->Execute();
-        }
-        else
-        {
-            logprintf("rpc failed");
-        }
-    };
+    void Execute() override;
 
 private:
     std::function<std::shared_ptr<CCallback>(AsyncClientCall<T>&, CError<CCallback>&)> GetCallback;
